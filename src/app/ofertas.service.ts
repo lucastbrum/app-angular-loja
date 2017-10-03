34fc614ef19896dc427a803947c9d121321a1845
Lucas Brum <lucas_trabach@hotmail.com>
2017-10-03 15:10:49 -0300

    Efetuando requisições HTTP para API Rest

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,63 +1,23 @@
+import { Http } from '@angular/http'
+import { Injectable } from '@angular/core'
 import { Oferta } from './shared/oferta.model'
 
+import 'rxjs/add/operator/toPromise'
+
+@Injectable()
 export class OfertasService {
 
-    public ofertas: Array<Oferta> = [
-        {
-            id: 1,
-            categoria: "restaurante",
-            titulo: "Super Burger",
-            descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
-            anunciante: "Original Burger",
-            valor: 29.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/1/img1.jpg"},
-                {url: "/assets/ofertas/1/img2.jpg"},
-                {url: "/assets/ofertas/1/img3.jpg"},
-                {url: "/assets/ofertas/1/img4.jpg"}
-            ]
-        },
-        {
-            id: 2,
-            categoria: "restaurante",
-            titulo: "Cozinha Mexicana",
-            descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
-            anunciante: "Mexicana",
-            valor: 32.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/2/img1.jpg"},
-                {url: "/assets/ofertas/2/img2.jpg"},
-                {url: "/assets/ofertas/2/img3.jpg"},
-                {url: "/assets/ofertas/2/img4.jpg"}
-            ]
-        
-        },
-        {
-            id: 4,
-            categoria: "diversao",
-            titulo: "Pizzaria",
-            descricao_oferta: "Almoço ou Jantar com Rodízio de pizza delicioso.",
-            anunciante: "Estância das águas",
-            valor: 31.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/3/img1.jpg"},
-                {url: "/assets/ofertas/3/img2.jpg"},
-                {url: "/assets/ofertas/3/img3.jpg"},
-                {url: "/assets/ofertas/3/img4.jpg"},
-                {url: "/assets/ofertas/3/img5.jpg"},
-                {url: "/assets/ofertas/3/img6.jpg"}
-            ]
-        }
-    ]
-
-    public getOfertas(): Array<Oferta>{
-        return this.ofertas
+    constructor(private http: Http){}
+    
+    public getOfertas(): Promise<Oferta[]>{
+        //efetuar uma requisição http
+        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+            .toPromise()
+            .then((resposta: any) => resposta.json())
+        //retornar uma promise Oferta[]
     }
 
-    public getOfertas2(): Promise<Oferta[]>{
+    /*public getOfertas2(): Promise<Oferta[]>{
         return new Promise((resolve, reject) => {
             //algum tipo de processamento, que ao finalizar, chama a função resolve
             let deu_certo = true
@@ -83,5 +43,5 @@ export class OfertasService {
             console.log('terceiro then executado após 3 segundos, estava aguardando uma promise ser resolvida')
             return ofertas
         })
-    }
+    }*/
 }
