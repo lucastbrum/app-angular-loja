5403b3efb8cc77f7d5d73fb78e2a9bb587e61fcd
Lucas Brum <lucas_trabach@hotmail.com>
2017-10-05 15:07:23 -0300

    Rotas

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,51 +2,32 @@ import { Http } from '@angular/http'
 import { Injectable } from '@angular/core'
 import { Oferta } from './shared/oferta.model'
 
+import { URL_API } from './app.api'
+
 import 'rxjs/add/operator/toPromise'
 
 @Injectable()
 export class OfertasService {
-
+    
     constructor(private http: Http){}
     
-    public getOfertas(): Promise<Oferta[]>{
-        //efetuar uma requisição http
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+    public getOfertas(): Promise<Oferta[]> {
+        return this.http.get(`${URL_API}?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta.json())
     }
 
     public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]> {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
             .toPromise()
             .then((resposta: any) => resposta.json())
     }
 
-    /*public getOfertas2(): Promise<Oferta[]>{
-        return new Promise((resolve, reject) => {
-            //algum tipo de processamento, que ao finalizar, chama a função resolve
-            let deu_certo = true
-            if(deu_certo){
-                setTimeout(() => resolve( this.ofertas ), 3000)
-            } else{
-                reject({ codigo_erro: 404, mensagem_erro: 'Servidor não encontrado'})
-            }
-        })
-        .then(( ofertas: Oferta[] ) => {
-            //fazer alguma tratativa
-            console.log('primeiro then')
-            return ofertas
-        })
-        .then(( ofertas: Oferta[] ) => {
-            //fazer outra tratativa
-            console.log('segundo then')
-            return new Promise((resolve2, reject2) => {
-                setTimeout(() => { resolve2( ofertas ) }, 3000)
+    public getOfertaPorId(id: number): Promise<Oferta> {
+        return this.http.get(`${URL_API}?id=${id}`)
+            .toPromise()
+            .then((resposta: any) => {
+                return resposta.json()[0]
             })
-        })
-        .then(( ofertas: Oferta[] ) => {
-            console.log('terceiro then executado após 3 segundos, estava aguardando uma promise ser resolvida')
-            return ofertas
-        })
-    }*/
+    }
 }
